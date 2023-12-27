fb1b16a0d1374ec517566529744209418f8322b9
Chandrahas Patel <70541620+chandrahas-46@users.noreply.github.com>
2023-12-27 21:05:02 +0530

    Update surveyForm.routes.js

--- backend/src/routes/surveyForm.routes.js
+++ backend/src/routes/surveyForm.routes.js
@@ -1,10 +1,19 @@
 // 1. Import express.
 import express from 'express';
 import SurveyController from '../controllers/surveyForm.controller.js';
+import cors from 'cors';
 
 // 2. Initialize Express router.
 const surveyRouter = express.Router();
 const surveyController = new SurveyController();
+
+surveyRouter.use(cors(
+    {
+        origin: ["https://survey-form-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 
 
 // All the paths to the controller methods.
