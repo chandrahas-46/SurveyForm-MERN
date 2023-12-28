b0b5e87a6da21fe655542dbbe478602bd5033a2a
Chandrahas Patel <70541620+chandrahas-46@users.noreply.github.com>
2023-12-28 20:23:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,13 @@ const surveyController = new SurveyController();
 
 const app = express();
 // app.use(cors());
-// app.use(cors(
-//     {
-//         origin: '*'
-//     }
-// ));
+app.use(cors(
+    {
+        origin: ["https://survey-form-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 // app.use("/", adminRouter);
