7b352bbbec56f876cdda568d70da94a6dbf5af67
Chandrahas Patel <70541620+chandrahas-46@users.noreply.github.com>
2023-12-29 18:14:32 +0530

    Update Signup.js

--- frontend/src/Component/Signup.js
+++ frontend/src/Component/Signup.js
@@ -17,7 +17,7 @@ export function Signup(){
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post(`http://localhost:3001/register`, {name, email, password})
+        axios.post(`https://survey-form-mern-api.vercel.app/register`, {name, email, password})
         .then(result => {
             console.log("FrontEnd SignUp: ",result);
             toast.success('Registration successful!');
